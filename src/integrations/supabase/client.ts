a1ed929c7a3c8d99909de4f4be6612e953aa242b
JMHIII <38891752+hendoctor@users.noreply.github.com>
2025-07-20 19:32:48 -0500

    Fix asset base path and config

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,8 +3,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://ylewpehqfgltbhpkaout.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlsZXdwZWhxZmdsdGJocGthb3V0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4NDkzMTcsImV4cCI6MjA2NTQyNTMxN30.ljNvNQSG0A2RsRkW8WXxADiOqVmZywD2GX318riotXs";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
