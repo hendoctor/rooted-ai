890da6dc6dd2d151fec0d5a428309ad516f0523d
JMHIII <38891752+hendoctor@users.noreply.github.com>
2025-09-17 08:08:49 -0500

    style: make newsletter card title green

--- src/components/client-portal/NewsletterSubscriptionCard.tsx
+++ src/components/client-portal/NewsletterSubscriptionCard.tsx
@@ -52,8 +52,8 @@ export const NewsletterSubscriptionCard = ({ companyId }: NewsletterSubscription
   return (
     <Card>
       <CardHeader>
-        <CardTitle className="flex items-center gap-2">
-          <Mail className="h-5 w-5" />
+        <CardTitle className="flex items-center gap-2 text-forest-green">
+          <Mail className="h-5 w-5 text-forest-green" />
           Newsletter Subscription
         </CardTitle>
         <CardDescription>
